3ff75ac291693c67d35ec48c88269e5439d77cc9
root <root@localhost.(none)>
2013-12-20 22:25:22 -0500

    just some initial ideas

---
+++ postCroutonInstall.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+
+#general tasks
+sudo apt-get update
+sudo apt-get install xubuntu-restricted-extras
+sudo apt-get install xfce4-goodies
+
+
+#install productivity applications
+sudo apt-get install firefox
+sudo apt-get install chromium-browser
+sudo apt-get install libreoffice
+sudo apt-get install synapse
+
+
+#install development tools
+##########################
+
+#guake command line
+sudo apt-get install guake
+
+#sublime text
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt-get install sublime-text
+
+#lamp server
+sudo apt-get install lamp-server^
+sudo apt-get install phpmyadmin
+sudo service apache2 restart
+
+#git
+sudo apt-get install git-core
+git config --global core.editor "vi"
+
+#sass/compass
+sudo apt-get install ruby1.9.1
+sudo gem install sass
+sudo gem install compass
+
+#grab script for spinning new sites
+cd /usr/local/bin
+sudo git clone https://github.com/milodesc/createNewDevSite.git
+
+
+#install design tools
+#####################
+
+#gimp -- raster graphics
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt-get update
+sudo apt-get install gimp gimp-gmic gmic
+sudo apt-get install gimp-plugin-registry
+sudo apt-get install gimp-resynthesizer
+
+#inkscape -- vector graphics
+sudo apt-get install inkscape
